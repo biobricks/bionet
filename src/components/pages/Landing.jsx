a8fe784890c2e5ff13ac9bbae83279ed90d6d698
Matthew Stewart <matthew.stewart.mi@gmail.com>
2019-02-06 15:13:34 -0800

    update landing

--- src/components/pages/Landing.jsx
+++ src/components/pages/Landing.jsx
@@ -1,14 +1,14 @@
-import React, { Component, Suspense, lazy } from 'react'; 
+import React, { Component, lazy } from 'react'; 
 import { Link } from 'react-router-dom';
 import FadeIn from 'react-fade-in';
 import DataPanel from '../DataPanel';
-
+import SearchBar from '../SearchBar';
 
 const Container = lazy(() => import('../bootstrap/grid/Container'));
 const Row = lazy(() => import('../bootstrap/grid/Row'));
 const Column = lazy(() => import('../bootstrap/grid/Column'));
 const Card = lazy(() => import('../bootstrap/components/Card'));
-const SearchBar = lazy(() => import('../SearchBar'));
+//const SearchBar = lazy(() => import('../SearchBar'));
 //const DataPanel = lazy(() => import('../DataPanel'));
 
 class Landing extends Component {
@@ -20,35 +20,37 @@ class Landing extends Component {
 
   render() {
     const selectedRecord = this.props.selectedRecord;
+    const selectedRecordExists = selectedRecord && Object.keys(selectedRecord).length > 0;
     return (
       <div className="Landing">
-        <Suspense fallback="Loading...">
           <FadeIn>
             <SearchBar {...this.props} />
             <Container className="pb-3">
               <Row>
                 <Column col="12" colLg="7">
-                  <Card icon="home" title="Home" className="mt-3">
-                    {this.props.isLoggedIn ? (
-                      <>
+                  { !selectedRecordExists ? (
+                    <Card icon="home" title="Home" className="mt-3">
+                      {this.props.isLoggedIn ? (
                         <p className="card-text">Welcome to Bionet <strong className="text-capitalize">{this.props.currentUser.username}</strong>.</p>
-                      </>
-                    ) : (
-                      <>
-                        <p className="card-text">Welcome to Bionet.</p>
-                        <Link className="card-link" to="/login">Login</Link>
-                        <Link className="card-link" to="/signup">Sign Up</Link>
-                      </>
-                    )}
-                  </Card> 
-                  {selectedRecord && (
-                    <DataPanel {...this.props} />
-                  )} 
+                      ) : (
+                        <>
+                          <p className="card-text">Welcome to Bionet.</p>
+                          <Link className="card-link" to="/login">Login</Link>
+                          <Link className="card-link" to="/signup">Sign Up</Link>
+                        </>
+                      )}
+                    </Card> 
+                  ) : (
+                    <>  
+                      {selectedRecord && (
+                        <DataPanel {...this.props} />
+                      )} 
+                    </>
+                  )}    
                 </Column>  
               </Row>
             </Container>
           </FadeIn>
-        </Suspense>
       </div>
     );
   }
