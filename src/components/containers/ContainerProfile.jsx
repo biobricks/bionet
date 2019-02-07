2eaee8f956756fea3d1db680d604d3d398355822
Matthew Stewart <matthew.stewart.mi@gmail.com>
2019-02-06 22:59:06 -0800

    add moment date wrappers

--- src/components/containers/ContainerProfile.jsx
+++ src/components/containers/ContainerProfile.jsx
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 
 class ContainerProfile extends Component {
   render () {
     const selectedRecord = this.props.selectedRecord;
+    const createdAtWrapper = moment(selectedRecord.createdAt);
+    const createdAt = createdAtWrapper.format("MM/DD/YY");
+    const updatedAtWrapper = moment(selectedRecord.updatedAt);
+    const updatedAt = updatedAtWrapper.format("MM/DD/YY");
+    const datesMatch = String(createdAt) === String(updatedAt);
     return (
       <div className="ContainerProfile">
-        <div className="form-row">
+        <div className="form-row mb-1">
+          <div className="col-12">
+            <label className="mb-0">
+              Created {createdAt}
+              {!datesMatch && <>&nbsp;-&nbsp;Last Updated {updatedAt}</>}
+            </label>
+          </div> 
+        </div>
+        <div className="form-row mb-1">
           <div className="col-12 col-md-3 col-lg-2">
-          <label>Description</label>
+            <label className="mb-0">Description</label>
           </div>
           <div className="col-12 col-md-9 col-lg-10">
             {selectedRecord.description}
--- src/components/virtuals/VirtualProfile.jsx
+++ src/components/virtuals/VirtualProfile.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 
 class VirtualProfile extends Component {
   render () {
     const selectedRecord = this.props.selectedRecord;
+    const createdAtWrapper = moment(selectedRecord.createdAt);
+    const createdAt = createdAtWrapper.format("MM/DD/YY");
+    const updatedAtWrapper = moment(selectedRecord.updatedAt);
+    const updatedAt = updatedAtWrapper.format("MM/DD/YY");
+    const datesMatch = String(createdAt) === String(updatedAt);
     return (
       <div className="VirtualProfile">
+        <div className="form-row mb-1">
+          <div className="col-12">
+            <label className="mb-0">
+              Created {createdAt}
+              {!datesMatch && <>&nbsp;-&nbsp;Last Updated {updatedAt}</>}
+            </label>
+          </div> 
+        </div>         
         <div className="form-row mb-1">
           <div className="col-12 col-md-3 col-lg-2">
             <label className="mb-0">Description</label>
